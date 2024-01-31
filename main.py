e40a1efff79e274aa3e7c9af43bdb0225f95d018
Albobob <treblarpip@gmail.com>
2024-01-31 10:58:01 +0300

    PEP 8 Code Refinement

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from openpyxl import load_workbook
 import string
 from statistics import mean
 
-FILE = 'СМП 10 2023.xlsx'
-REC_COL = 'L'
-REC_COL_MR = 'M'
-OUTLIERS_FLAG = 'N'
-START_END_COll = [2, 10]
-START_END_ROW = [11, 104]
+FILE: str = 'СМП 10 2023.xlsx'
+REC_COL: str = 'L'
+REC_COL_MR: str = 'M'
+OUTLIERS_FLAG: str = 'N'
+START_END_COll: list[int, int] = [2, 10]
+START_END_ROW: list[int, int] = [11, 104]
 
 
-def decode(work_book, sheet, coll, rows):
+def decode(work_book: str, sheet: str, coll: str, rows: int) -> float:
     """ Функция для чтения из EXEL. На вход подается:
  work_book = файл EXEL,
  sheet = страница,
@@ -18,32 +18,33 @@ def decode(work_book, sheet, coll, rows):
  coll = колонка,
  rows = строчка
     """
-    sheet = work_book[f'{sheet}']
-    value_cell = sheet[f'{coll}{rows}'].value
+    sheet: str = work_book[f'{sheet}']
+    value_cell: float = sheet[f'{coll}{rows}'].value
     return value_cell
 
 
-def record(work_book, sheet, coll, rows, value):
+def record(work_book: str, sheet: str, coll: str, rows: int,
+           value: float) -> None:
     """ Функция для записи в EXEL. На вход подается:
         work_book = файл EXEL,
         sheet = страница,
         коардинаты ячейки(колонка и строчка)
         coll = колонка,
         rows = строчка
-        значение = value (value преобразуется в тип float)
+        значение = value (value имеет тип float)
     """
-    sheet = work_book[f'{sheet}']
+    sheet: str = work_book[f'{sheet}']
     sheet[f'{coll}{rows}'] = value
 
 
 wb = load_workbook(filename=FILE)
-sheet = wb.sheetnames
+sheet: str = wb.sheetnames
 
-coll = list(string.ascii_lowercase)
+coll: list = list(string.ascii_lowercase)
 
 
-def get_data(sheet, row):
-    data = []
+def get_data(sheet: str, row: int) -> list[float]:
+    data: list[float] = []
 
     for c in coll[START_END_COll[0] - 1:START_END_COll[1] + 1]:
         vl = decode(wb, sheet, c, row)
@@ -53,12 +54,12 @@ def get_data(sheet, row):
     return data
 
 
-def smp(data):
+def smp(data: list) -> float:
     try:
-        ls = []
+        ls: list[float] = []
         for i in data:
-            mx = max(data)
-            mn = min(data)
+            mx: float = max(data)
+            mn: float = min(data)
             if i != mx and i != mn:
                 ls.append(i)
         if len(ls) > 0:
@@ -71,7 +72,7 @@ def smp(data):
 
 def check_outliers(data: list):
     # Критерии для исключения выскакивающих вариант
-    exclusion_criteria = {
+    exclusion_criteria: dict[int, dict[int, float]] = {
         3: {95: 0.941, 99: 0.988},
         4: {95: 0.765, 99: 0.889},
         5: {95: 0.642, 99: 0.78},
@@ -88,42 +89,45 @@ def check_outliers(data: list):
         30: {95: 0.26, 99: 0.341}
     }
     # Уровень достоверности, %
-    a = exclusion_criteria[len(data)][95]
-    b = exclusion_criteria[len(data)][99]
+    # TODO: Переименовать переменные a, b
+    a: float = exclusion_criteria[len(data)][95]
+    b: float = exclusion_criteria[len(data)][99]
 
     # Построение вариант в порядке возрастания
     data.sort()
+
     # Вычисление разности между наибольшей и наименьшей вариантами ряда
-    diff_max_min = max(data) - min(data)
-    outlier = set()
-    rec_xl = {
+    diff_max_min: float = max(data) - min(data)
+    outlier: set = set()
+
+    rec_xl: dict[str, bool] = {
         'outliers_max': None,
         'outliers_min': None,
         'value': None,
     }
+
     try:
-        ratio_max = (data[-1] - data[-2]) / diff_max_min
+        ratio_max: float = (data[-1] - data[-2]) / diff_max_min
         if ratio_max > a and ratio_max > b:
             outlier.add(max(data))
             rec_xl['outliers_max'] = 'y'
             print(f'Есть выскакивающий показатель {max(data)}')
 
-        ratio_min = (data[1] - data[0]) / diff_max_min
+        ratio_min: float = (data[1] - data[0]) / diff_max_min
         if ratio_min > a and ratio_min > b:
             outlier.add(min(data))
             rec_xl['outliers_min'] = 'y'
             print(f'Есть выскакивающий показатель {min(data)}')
 
-            # Отладка
-            print(f'{data[1]} - {data[0]}\n------------------\n{ratio_min}\n'
-                  f'{round(ratio_min, 2)}>{a}\n{round(ratio_min, 2)}>{b}')
-            print(f'{data}\n{set(data) - outlier}')
-
+            # # Отладка
+            # TODO Продумать отладку отдельной функцией
+            # print(f'{data[1]} - {data[0]}\n------------------\n{ratio_min}\n'
+            #       f'{round(ratio_min, 2)}>{a}\n{round(ratio_min, 2)}>{b}')
+            # print(f'{data}\n{set(data) - outlier}')
 
     except ZeroDivisionError:
         rec_xl['value'] = 0
 
-    # print(rec_xl)
     rec_xl['value'] = round(mean(set(data) - outlier), 3)
     return rec_xl
 
@@ -131,15 +135,26 @@ def check_outliers(data: list):
 # ЗАПИСЬ В ФАЙЛ
 for sh in sheet:
     for row in range(START_END_ROW[0], START_END_ROW[1] + 1):
-        data = get_data(sh, row)
+        data: list[float] = get_data(sh, row)
+        # Записываем заголовки граф
         record(wb, sh, REC_COL, START_END_ROW[0] - 1, 'CМП (выброс макс мин)')
         record(wb, sh, REC_COL_MR, START_END_ROW[0] - 1, 'CМП (по МР)')
+        record(wb, sh, OUTLIERS_FLAG, START_END_ROW[0] - 1,
+               'Есть выскакивающий показатель')
+
         if len(data) >= 3:
-            if check_outliers(data)['outliers_max'] == 'y' or check_outliers(data)['outliers_min'] == 'y':
-                record(wb, sh, OUTLIERS_FLAG, START_END_ROW[0] - 1, 'Есть выскакивающий показатель')
+
+            # Записываем СМП (по МР)
+            record(wb, sh, f'{REC_COL_MR}', row,
+                   check_outliers(data)['value'])
+
+            # Записываем Да если есть выскакивающий показатель
+            if check_outliers(data)['outliers_max'] == 'y' or check_outliers(
+                    data)['outliers_min'] == 'y':
                 record(wb, sh, f'{OUTLIERS_FLAG}', row, 'Да')
-            record(wb, sh, f'{REC_COL_MR}', row, check_outliers(data)['value'])
+
         if smp(data) > 0:
+            # Записываем СМП (стырый способ)
             record(wb, sh, f'{REC_COL}', row, smp(data))
         else:
             record(wb, sh, f'{REC_COL}', row, 0)
